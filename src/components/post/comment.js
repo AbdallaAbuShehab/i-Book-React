89a20dab6dbaf671de50d2d955ea99221741eb7f
Abdalla Abu Shehab <90116279+AbdallaAbuShehab@users.noreply.github.com>
2024-02-22 00:39:24 +0300

    Update comment.js

--- src/components/post/comment.js
+++ src/components/post/comment.js
@@ -2,7 +2,7 @@ export default function Comment({ comment }) {
   return (
     <div className="flex flex-row bg-amber-200 p-2 rounded-md m-3 shadow-md hover:bg-amber-100 transition duration-300">
       <div>
-        <span className=" text-md font-semibold text-neutral-800 dark:text-neutral-200 rounded-md px-1.5 hover:bg-amber-200 transition duration-300 cursor-pointer  py-0.1 bg-amber-300 shadow-md ">
+        <span className=" text-md font-semibold text-neutral-800 text-black rounded-md px-1.5 hover:bg-amber-200 transition duration-300 cursor-pointer  py-0.1 bg-amber-300 shadow-md ">
           {comment.name.slice(0, 10)}
         </span>
 
